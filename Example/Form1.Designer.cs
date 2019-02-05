8e45fc21f99e2b7a091cbab55960313800dbf63c
Sahan S Liyanage <sahanslst@gmail.com>
2019-02-06 03:02:00 +0530

    initial commit

---
+++ Example/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace Example
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtindexno = new System.Windows.Forms.TextBox();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.txtaddress = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtindexno
+            // 
+            this.txtindexno.Location = new System.Drawing.Point(285, 27);
+            this.txtindexno.Name = "txtindexno";
+            this.txtindexno.Size = new System.Drawing.Size(253, 20);
+            this.txtindexno.TabIndex = 0;
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(285, 84);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(253, 20);
+            this.txtname.TabIndex = 0;
+            // 
+            // txtaddress
+            // 
+            this.txtaddress.Location = new System.Drawing.Point(285, 143);
+            this.txtaddress.Name = "txtaddress";
+            this.txtaddress.Size = new System.Drawing.Size(253, 20);
+            this.txtaddress.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(204, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(59, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "INDEX NO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(222, 84);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "NAME";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(203, 150);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "ADDRESS";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(207, 227);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Previous";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(497, 227);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Next";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtaddress);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.txtindexno);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtindexno;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.TextBox txtaddress;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Example/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Example
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        System.Data.SqlClient.SqlConnection con;
+        DataSet ds1;
+        System.Data.SqlClient.SqlDataAdapter da;
+        int MaxRows = 0;
+        int inc = 0;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            con = new System.Data.SqlClient.SqlConnection();
+            ds1 = new DataSet();
+            con.ConnectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\\example.mdf;Integrated Security=True";
+
+            string sql = "SELECT*From example";
+            da = new System.Data.SqlClient.SqlDataAdapter(sql, con);
+            da.Fill(ds1, "example");
+            NavigateRecords();
+            MaxRows = ds1.Tables["example"].Rows.Count;
+           con.Open();
+         //  MessageBox.Show("Database Opened");
+          
+         con.Close();
+          // MessageBox.Show("Database Closed");
+          //  con.Dispose();
+        }
+            private void NavigateRecords()
+            {
+            DataRow dRow = ds1.Tables["example"].Rows[inc];
+                txtindexno.Text = dRow.ItemArray.GetValue(0).ToString();
+                txtname.Text = dRow.ItemArray.GetValue(1).ToString();
+                txtaddress.Text = dRow.ItemArray.GetValue(2).ToString();
+            }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (inc !=MaxRows-1)
+            {
+                inc++;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("No more Rows");
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(inc>0)
+            {
+                inc--;
+                NavigateRecords();
+            }
+            else
+            {
+                MessageBox.Show("This is First Record");
+            }
+        }
+    }
+    }
+
